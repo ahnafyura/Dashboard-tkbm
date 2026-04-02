3cba95d98db60f1e3fb0effc6e7ad6f352372a79
ahnafyura <ahnafalghiffarriahtasyafi@gmail.com>
2026-04-02 15:12:48 +0700

    fix: move storage path inside bootstrap app correctly

--- api/index.php
+++ api/index.php
@@ -1,27 +1,4 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 1. Paksa Laravel menggunakan folder /tmp (Memory Vercel)
-$app->useStoragePath('/tmp/storage');
-
-// 2. Buat otomatis kerangka folder yang dibutuhkan Laravel di dalam /tmp
-$dirs = [
-    '/tmp/storage/logs',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/bootstrap/cache',
-];
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0777, true);
-    }
-}
-
-// 3. Jalankan Website!
-$request = Illuminate\Http\Request::capture();
-$response = $app->handleRequest($request);
-$response->send();
-$app->terminate($request, $response);
+// Masuk ke folder public/index.php
+require __DIR__ . '/../public/index.php';

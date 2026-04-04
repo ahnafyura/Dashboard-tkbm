29f8d9e7fb0c91059730f5302744b48322e1806b
me <175010308+chiisana-riryi@users.noreply.github.com>
2026-04-04 22:17:34 +0700

    summary cards partials

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
 <body class="base-background-color">
     <div class="d-flex">
 
-        @include('templates.sidebar')
+        @include('partials.sidebar')
 
         <div class="container-fluid p-2">
 
@@ -41,43 +41,7 @@
                     </div>
                 </div>
 
-                {{-- summary cards row --}}
-                <div class="row mt-5 align-items-stretch d-flex">
-                    {{-- <div class="col-6">
-                <div class="card base-font-color base-card-background-color  shadow rounded-3 p-3 h-100">
-                    <h4> Zona Panas </h4>
-
-                    <img class="img-fluid rounded-3" src="https://picsum.photos/seed/picsum/800/300">
-                </div>
-            </div> --}}
-
-                    <div class="col-3">
-                        <div class="card border-2 base-border-color base-font-color base-card-background-color  min-card-size shadow rounded-3 p-3 h-100">
-                            <h4 class="mb-3">Active Incidents</h4>
-                            <div class="d-flex justify-content-center align-items-center text-center h-100">
-                                <div>
-                                    <h1 id="active-incidents-counter" class="mb-3"> ... </h1>
-                                    {{-- <div class="card base-card-background-color  rounded-5 p-2">0 Critical</div> --}}
-                                </div>
-                            </div>
-
-                            {{-- <div class="row d-flex justify-content-between"> 
-                        <div class="col-auto"> Last updated: </div>
-                        <div class="col"> PLACEHOLDER-TIME </div>
-                    </div> --}}
-                        </div>
-                    </div>
-
-                    <div class="col-3">
-                        <div class="card border-2 base-border-color base-font-color base-card-background-color  min-card-size shadow rounded-3 p-3 h-100">
-                            <h4> Average Fatigue </h4>
-
-                            <div class="d-flex justify-content-center align-items-center h-100">
-                                <h1 id="avg-fatigue"> ... </h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('partials.summary_cards')
 
                 {{-- telemetrys --}}
                 <div class="card border-2 base-border-color base-font-color base-card-background-color shadow mt-5 p-3">
---
+++ resources/views/partials/summary_cards.blade.php
@@ -0,0 +1,27 @@
+{{-- summary cards row --}}
+<div class="row mt-5 align-items-stretch d-flex">
+
+    <div class="col-3">
+        <div
+            class="card border-2 base-border-color base-font-color base-card-background-color  min-card-size shadow rounded-3 p-3 h-100">
+            <h4 class="mb-3">Active Incidents</h4>
+            <div class="d-flex justify-content-center align-items-center text-center h-100">
+                <div>
+                    <h1 id="active-incidents-counter" class="mb-3"> ... </h1>
+                    {{-- <div class="card base-card-background-color  rounded-5 p-2">0 Critical</div> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-3">
+        <div
+            class="card border-2 base-border-color base-font-color base-card-background-color  min-card-size shadow rounded-3 p-3 h-100">
+            <h4> Average Fatigue </h4>
+
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <h1 id="avg-fatigue"> ... </h1>
+            </div>
+        </div>
+    </div>
+</div>
